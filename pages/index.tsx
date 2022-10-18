1bcefba7d3356f7b96661a054e9c03f61fea4b3b
Gál Péter <aimd51@inf.elte.hu>
2022-10-18 09:23:45 +0200

    feat: show teams on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import TeamCreateButton from "@components/TeamCreateButton";
 import TeamList from "@components/TeamList";
 import { useAuth, useUser } from "@components/UserProvider";
-import { Card, CardContent, Container, Link, Typography } from "@mui/material";
+import { ExitToAppRounded as LogoutIcon } from "@mui/icons-material";
+import { Button, Card, CardActions, CardContent, Container, Grid, Link, Typography } from "@mui/material";
 import NextLink from "next/link";
 
 const HomePage = () => {
@@ -13,7 +14,7 @@ const HomePage = () => {
         logout();
     };
 
-    if(!user) {
+    if (!user) {
         return (
             <Container maxWidth={"sm"}>
                 You are not logged in! <NextLink href={"/auth/login"}>Log in!</NextLink>
@@ -23,19 +24,39 @@ const HomePage = () => {
 
     return (
         <Container maxWidth={"sm"}>
-            Hello, {user.username}! <Link
-                href={"#"}
-                onClick={handleLogout}
-            >Logout</Link>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant={"h5"} gutterBottom>
+                                Hello, {user.username}!
+                            </Typography>
+                            <Typography>
+                                {"You are currently viewing the dashboard of SYGNMNT, an application to create and test students' code submissions."}
+                            </Typography>
+                            <Typography gutterBottom>
+                                
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button onClick={handleLogout} startIcon={<LogoutIcon />}>
+                                Logout
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={12}>
 
-            <Card>
-                <CardContent sx={{ pb: 0 }}>
-                    <Typography variant={"h5"} gutterBottom>
-                        Teams
-                    </Typography>
-                </CardContent>
-                <TeamList />
-            </Card>
+                    <Card>
+                        <CardContent sx={{ pb: 0 }}>
+                            <Typography variant={"h5"} gutterBottom>
+                                Teams
+                            </Typography>
+                        </CardContent>
+                        <TeamList />
+                    </Card>
+                </Grid>
+            </Grid>
             <TeamCreateButton
                 color={"primary"}
                 sx={{
