46d66feecb4b98f2f015efadf7e0f3739bb15c25
Tuguldur Sosorbaram <eerdey@inf.elte.hu>
2022-10-19 15:53:01 +0200

    Update [id].tsx

--- pages/api/team/[id].tsx
+++ pages/api/team/[id].tsx
@@ -4,12 +4,10 @@ import { middleware } from "@lib/server/middleware";
 import prisma from "@lib/server/prisma";
 import { NextApiRequest } from "next";
 import { TeamType } from "../team";
-
 export type GetTeamByIdResponseType = TeamType;
-
 /*
- * Gets single team based on ID - whom the user must be a member of.
- */
+* Gets single team based on ID - whom the user must be a member of.
+*/
 const getTeamById = async (req: NextApiRequest): Promise<GetTeamByIdResponseType> => {
     const user = await Authentication.getUser(req);
     if(!user) throw new UnauthorizedError();
@@ -41,7 +39,6 @@ const getTeamById = async (req: NextApiRequest): Promise<GetTeamByIdResponseType
     if(!membership) {
         throw new Error("This team does not exist or you are not a member of it!");
     }
-
     return {
         name: membership.team.name,
         role: membership.role,
@@ -52,13 +49,65 @@ const getTeamById = async (req: NextApiRequest): Promise<GetTeamByIdResponseType
         })),
     };
 };
-
+const updateTeamById = async (req: NextApiRequest) => {
+    const user = await Authentication.getUser(req);
+    if(!user) throw new UnauthorizedError();
+    
+    const id = req.query.id as string;
+    const { name } = JSON.parse(req.body);
+    if(!id || id.length !== 36) {
+        // TODO: Use regex to determinate if the given ID is a UUID as this feels hacky.
+        throw new Error("A team's ID must be exactly 36 characters long!");
+    }
+    if(!name || name.length === 0) {
+        throw new Error("A team's name must be at least 1 character long!");
+    }
+    
+    const membership = await prisma.membership.findFirst({
+        where: {
+            userId: user.id,
+            teamId: id,
+        },
+        include: {
+            team: {
+                include: {
+                    memberships: {
+                        include: {
+                            user: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+    if(!membership) {
+        throw new Error("Team not found!");
+    }
+    if(membership.role !== "OWNER") {
+        throw new Error("You don't have permission for this!");
+    }
+    await prisma.team.update({
+        where: {
+            id: membership.teamId,
+        },
+        data: {
+            name,
+        },
+    });
+    const team = await getTeamById(req);
+    return {
+        message: "Team successfully updated!",
+        team,
+    };
+};
 /**
- * This is the controller for the Team resources.
- */
+* This is the controller for the Team resources.
+*/
 const controller = async (req: NextApiRequest) => {
     if(req.method === "GET") {
         return await getTeamById(req);
+    } if(req.method === "PATCH") {
+        return await updateTeamById(req);
     } else {
         throw new UnsupportedMethodError();
     }
