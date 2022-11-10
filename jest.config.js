0fbe815a22fd7b649f282d0af05a0cdd94cbfde7
Gál Péter <aimd51@inf.elte.hu>
2022-11-10 09:26:35 +0100

    fix: test fails because of tsconfig paths

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' }),
 };
