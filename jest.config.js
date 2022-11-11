30c877c2e0ebc3b7ce06122883045201a6765c44
Gál Péter <aimd51@inf.elte.hu>
2022-11-11 10:31:04 +0100

    chore: increase test timeout

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const { compilerOptions } = require('./tsconfig');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  testTimeout: 20000,
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' }),
