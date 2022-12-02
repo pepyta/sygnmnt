1ba90bf643c73b5c4b8ba880171eafe15de5d135
Gál Péter <aimd51@inf.elte.hu>
2022-12-02 13:51:23 +0100

    feat: show more information on submission details

--- components/SubmissionDetailsDialog.tsx
+++ components/SubmissionDetailsDialog.tsx
@@ -1,5 +1,6 @@
-import { Dialog, DialogContent, DialogProps, DialogTitle } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, Typography } from "@mui/material";
 import { ExtendedSubmissionType } from "@redux/slices/membership";
+import { useMemo } from "react";
 import SubmissionLog from "./SubmissionLog";
 
 export type SubmissionDetailsDialogProps = DialogProps & {
@@ -7,16 +8,32 @@ export type SubmissionDetailsDialogProps = DialogProps & {
 };
 
 const SubmissionDetailsDialog = ({ submission, ...props }: SubmissionDetailsDialogProps) => {
+    const date = useMemo(
+        () => {
+            const d = new Date(submission.createdAt);
+            return `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
+        },
+        [submission.createdAt],
+    );
+    
     return (
         <Dialog fullWidth maxWidth={"md"} {...props}>
             <DialogTitle>
-
+                Submission details
+                <Typography variant={"body2"}>
+                    Submitted by {submission.user.username} at {date}.
+                </Typography>
             </DialogTitle>
             <DialogContent>
                 <SubmissionLog
                     submission={submission}
                 />
             </DialogContent>
+            <DialogActions>
+                <Button onClick={() => props.onClose({}, "backdropClick")}>
+                    Close
+                </Button>
+            </DialogActions>
         </Dialog>
     );
 };
